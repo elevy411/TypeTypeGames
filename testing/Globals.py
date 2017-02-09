21779557d645124325b3f3189f16aa87cbb1064f
Elliot Levy <elevy411@cs.uchicago.edu>
2017-02-09 14:37:16 -0600

    I have added the abstraciton of Draw() and cleaned up a bunch of the code

--- testing/Globals.py
+++ testing/Globals.py
@@ -9,3 +9,11 @@ BLACK = (0, 0, 0)
 BLUE = (0,0,255)
 GREEN = (0,255,0)
 SPECIAL_KEYS = [pygame.K_ESCAPE,pygame.K_BACKSPACE,pygame.K_TAB,pygame.K_RETURN,pygame.K_SPACE]
+WORDLIST = []
+
+def make_word_list():
+	global WORDLIST
+	with open('wordList.txt') as f:
+		WORDLIST = f.read().splitlines()
+	return WORDLIST
+
