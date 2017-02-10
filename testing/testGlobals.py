acd0a60a8fc2533616653455ad3c107ce2a45c36
Coleman Smith <smithcd@cs.uchicago.edu>
2017-02-09 23:16:26 -0600

    adding menuItem.py

--- testing/testGlobals.py
+++ testing/testGlobals.py
@@ -8,8 +8,8 @@ def test_make_word_list():
         "Super","Awesome","Bingo","Damn","Flavorful"]
     assert len(test_list) == len(expected_list),"output list is not same length as expected list"
     for i in range(len(test_list)):
-        assert test_list[i] == expected_list[i],"indices do not match"
-        assert Globals.WORDLIST[i] == expected_list[i],"global indices do not match"
+        assert test_list[i] == expected_list[i],"returned list indices do not match"
+        assert Globals.WORDLIST[i] == expected_list[i],"global list indices do not match"
     print "no errors in make_word_list"
 
 if __name__ == "__main__":
