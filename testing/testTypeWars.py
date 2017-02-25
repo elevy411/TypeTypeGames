c026d0af4f740e2e69dfe56ec4bb6cbfa0fd8286
Gera Scott <gerascott@cs.uchicago.edu>
2017-02-24 23:49:15 -0600

    adding tests for Type Wars

--- testing/testTypeWars.py
+++ testing/testTypeWars.py
@@ -1,84 +1,97 @@
-import sys
-import pygame as P
-from word import Word
-from letter import Letter
+import game
 import Globals as G
-
-#+all import statements necessary 
-
-
-"""Series of tests for checking our implementation TypeTypeWars and Speed game
-   
-   player{
-	 index          #indicates current position of player in prompt
-	 counter        #time spent typing, tracked by pygames
-	 health         #initialized as 100, for combat
-	 
-	 modifyHealth() #only negate health
-	 modifyIndex()  #increase current index based on words typed 
-   	 calculateWPM() #calculated WPM for player
-   }"""
-
-#tests health modification
-def test_health_mod(player): 
-	#get health of player
-	health = player.health
-	player.modifyHealth(-1)
-	assert health - 1 == player.health
-
-	player.modifyHealth(-4)
-	assert health - 5 == player.health
-
-	#once player loses more than 100 health, 0
-	player.modifyHealth(-96)
-	assert 0 == player.health
-
-	#can't gain health, modifyHealth() only takes - ints
-	player.modifyHealth(20)
-	assert 0 == player.health
-
-#tests WPM calculation
-def test_WPM_calc(player):
-	typed = player.index
-	time_elapsed = player.counter
-	time_elapsed = time_elapsed / 60
-	WPM = player.calculateWPM()
-
-	assert WPM == typed / time_elapsed 
-
-#tests if typical combat sequence works
-def test_combat(player1, player2):
-	index1 = player1.index
-	index2 = player2.index
-	diff1 = index1 - index2
-	diff2 = index2 - index1
-	health1 = player1.health
-	health2 = player2.health
-
-	#someone loses health
-	player1.modifyHealth(diff1)
-	player2.modifyHealth(diff2)
-
-	if diff1 < 0:
-		assert health1 - diff1 = player1.health
-	if diff2 < 0:
-		assert health2 - diff2 == player2.health
-
-#tests if index modification works
-def test_index(player):
-	typed = player.index
-
-	player.modifyIndex(10)
-	assert typed + 10 = player.index
-
-	player.modifyIndex(100)
-	assert typed + 110 = player.index
+import Word 
+import Letter
+import TypeTypeWars as TW
 
 
+def test_set_speed():
+	# set difficulty level
+	G.set_difficulty_easy()
+
+	# set current speed based on difficulty level
+	TW.set_speed()
+
+	# get current speed
+	current_speed = TW.get_speed()
+
+	#change difficulty level
+	G.set_difficulty_hard()
+
+	# update speed
+	TW.set_speed()
+	updated_speed = TW.get_speed()
+
+	assert (current_speed < updated_speed), "speed didn't increase when difficulty level increased"
+
+	current_speed = TW.get_speed()
+
+	G.set_difficulty_medium()
+
+	TW.set_speed()
+	updated_speed = TW.get_speed()
+
+	assert (current_speed > updated_speed), "speed didn't decrease when difficulty level decreased"
 
 
+def test_add_to_attack():
+
+	# make random word list from larger list; different lists used depending on difficulty level
+	word_list = TW.make_word_list_from(G.WORDLIST)
+
+	curr_attack_list = TW.get_attack_list()
+
+	# add words to attack_list
+	for i in range(len(word_list)):
+		TW.add_to_attack(word_list[i])
+
+	updated_attack_list = TW.get_attack_list()
+
+	assert (len(curr_attack_list) < len(updated_attack_list)), "add_to_attack failed"
+	assert (len(updated_attack_list) == (len(curr_attack_list) + len(word_list))), "all words weren't added to attack list"
 
 
+def test_update_health():
+
+	test_word1 = Word.create_word("Test1")
+	test_word2 = Word.create_word("Test2")
+
+	# get initial health of player
+	current_health = TW.get_health()
+
+	# decrement health when incorrect word is typed
+	TW.update_health(test_word1.equals(test_word2))
+
+	# get updated health
+	updated_health = TW.get_health()
+
+	assert (current_health > updated_health), "health didn't decrease with incorrectly typed word"
+
+	current_health = TW.get_health()
+
+	# increment health when correct word is typed
+	TW.update_health(test_word2.equals(test_word2))
+
+	updated_health = TW.get_health()
+
+	assert (current_health < updated_health), "health didn't increase with correctly typed word"
 
 
+def test_reset_attack():
 
+	word_list = TW.make_word_list_from(G.WORDLIST)
+
+	# add words to attack_list
+	for i in range(len(word_list)):
+		TW.add_to_attack(word_list[i])
+
+	curr_attack_list = TW.get_attack_list()
+
+	assert (len(curr_attack_list) > 0), "add_to_attack failed"
+
+	TW.reset_attack_list()
+
+	curr_attack_list = TW.get_attack_list()
+
+	assert (len(curr_attack_list) == 0), "reset_attack_list failed"
+	
